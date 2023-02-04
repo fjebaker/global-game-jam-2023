295d86aa86d86d0f3409afa0cc5d90694c92c408
JR Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2023-02-04 19:11:20 +0000

    Fix/sfx stops working (#40)
    * chore: registers constant for the cart overflow modulo time
    
    * fix: ensures that the sound effect reports not playing across the tick time modulo boundry

--- cart/player.go
+++ cart/player.go
@@ -116,7 +116,7 @@ func (player *Player) HandleInteraction(t int32) {
 func (player *Player) Update(t int32, world *World) {
 	if player.Moving {
 		// check sfx update
-		if player.Move_fx.IsPlaying(t) == false {
+		if player.Move_fx.IsPlaying(t, OVERFLOW_MODULO_TIME) == false {
 			player.Move_fx.PlayRecordTime(t)
 		}
 		// check whether to advance location
