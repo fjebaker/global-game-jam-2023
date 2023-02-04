9d0b6479e299d2fc236950c3cc7904ba93c9f14c
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2023-02-04 23:51:15 +0000

    feat: adds an amount of time taken to eat

--- cart/player.go
+++ cart/player.go
@@ -12,20 +12,24 @@ const (
 
 	PLAYER_START_POSITION_X = 97*8 + 4*8
 	PLAYER_START_POSITION_Y = 14 * 8
+
+	DIRT_EAT_TIME = 10
 )
 
 type Player struct {
-	X, Y       int32
-	Frame      int32
-	Sprite     tic80.Sprite
-	Move_fx    tic80.SoundEffect
-	Eat_fx     tic80.SoundEffect
-	Speed      int32
-	Dead       bool
-	Digging    bool
-	Moving     bool
-	HasItem    bool
-	ItemSprite *tic80.Sprite
+	X, Y         int32
+	Frame        int32
+	Sprite       tic80.Sprite
+	Move_fx      tic80.SoundEffect
+	Eat_fx       tic80.SoundEffect
+	Speed        int32
+	Dead         bool
+	Digging      bool
+	Moving       bool
+	HasItem      bool
+	ItemSprite   *tic80.Sprite
+	Eating       bool
+	EatStartTime int32
 }
 
 func NewPlayer(worldX, worldY int32, desired_item_sprite *tic80.Sprite) Player {
@@ -34,7 +38,7 @@ func NewPlayer(worldX, worldY int32, desired_item_sprite *tic80.Sprite) Player {
 	move_fx := tic80.NewSoundEffect(61, 3, 8)
 	eat_fx := tic80.NewSoundEffect(63, 3, 30)
 
-	return Player{worldX, worldY, 0, sprite, move_fx, eat_fx, 4, false, false, false, false, desired_item_sprite}
+	return Player{worldX, worldY, 0, sprite, move_fx, eat_fx, 4, false, false, false, false, desired_item_sprite, false, 0}
 }
 
 const (
@@ -123,6 +127,10 @@ func (player *Player) HandleInteraction(t int32) {
 }
 
 func (player *Player) Update(t int32, world *World, game *Game) {
+	if player.isEating(t) {
+		return
+	}
+
 	if player.Moving {
 		// check sfx update
 		if player.Move_fx.IsPlaying(t, OVERFLOW_MODULO_TIME) == false {
@@ -147,7 +155,7 @@ func (player *Player) Update(t int32, world *World, game *Game) {
 		switch {
 		case world.IsDirt(tileIndex):
 			world.DigTile(x, y)
-			player.Eat_fx.Play()
+			player.startEating(t)
 		case world.IsItem(tileIndex):
 			world.CollectItem(x, y)
 		}
@@ -160,6 +168,21 @@ func (player *Player) Update(t int32, world *World, game *Game) {
 
 ///////////////////////////////////////////////////////////////////////////////
 // Utils
+
+func (player *Player) startEating(t int32) {
+	player.Eat_fx.Play()
+	player.Eating = true
+	player.EatStartTime = t
+}
+
+func (player *Player) isEating(t int32) bool {
+	if player.Eating {
+		if TimeSince(t, player.EatStartTime) >= DIRT_EAT_TIME {
+			player.Eating = false
+		}
+	}
+	return player.Eating
+}
 
 func (player *Player) incrementFrame() {
 	if !player.Dead {
