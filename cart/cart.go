23bdf51cb146aa5c187df060cd5f4488c9257278
fjebaker <fergusbkr@gmail.com>
2023-02-04 17:00:00 +0000

    feat: added rabbit into the world

--- cart/cart.go
+++ cart/cart.go
@@ -8,12 +8,14 @@ var (
 	_mouse  tic80.MouseData
 	_t      int32
 	_player Player
+	_rabbit Rabbit
 	_world  World
 )
 
 func Start() {
 	_t = 0
 	_player = NewPlayer(PLAYER_START_POSITION_X, PLAYER_START_POSITION_Y)
+	_rabbit = NewRabbit(100, 50, RABBIT_START_POSITION_X, RABBIT_START_POSITION_Y)
 	_world = NewWorld(&_player)
 }
 
@@ -24,9 +26,11 @@ func Loop() {
 
 	_player.HandleInteraction(_t)
 	_player.Update(_t, &_world)
+	_rabbit.Update(_t, &_player)
 	_world.Update(_t, &_player)
 
 	_world.Draw(_t)
+	_rabbit.Draw(_t)
 	_player.Draw(_t)
 
 	_t = _t + 1
