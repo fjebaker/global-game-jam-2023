d5a5eb7599ad93efe5d40dbfd726d7d986ebc9ca
Fergus Baker <fergusbkr@gmail.com>
2023-02-04 12:14:02 +0000

    Feat: added rabbit asset and fixed animation (#26)
    * fix(assets): rabbit animates correctly
    
    * feat: added rabbit struct

---
+++ cart/rabbit.go
@@ -0,0 +1,33 @@
+package cart
+
+import "cart/tic80"
+
+type Rabbit struct {
+	X, Y int32
+	MapX, MapY int32
+	Frame int32
+	Sprite tic80.Sprite
+}
+
+const rabbit_main_frame = 272
+
+func NewRabbit(x, y, mapx, mapy int32) Rabbit {
+	sprite := tic80.SquareSprite(rabbit_main_frame, 4)
+	return Rabbit{x, y, mapx, mapy, 0, sprite}
+}
+
+func (rabbit *Rabbit) switchIdleFrame() {
+	if rabbit.Frame == 0 {
+		rabbit.Frame = 1
+	} else {
+		rabbit.Frame = 0
+	}
+	rabbit.Sprite.Id = rabbit_main_frame + (4 * rabbit.Frame)
+}
+
+func (rabbit *Rabbit) Draw(t int32) {
+	if (t % 45 == 0) {
+		rabbit.switchIdleFrame()
+	}
+	rabbit.Sprite.Draw(rabbit.X, rabbit.Y)
+}
