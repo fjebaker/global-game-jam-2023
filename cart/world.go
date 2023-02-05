67b74cf9580a86d75cd8d4678397e2dd5d8c8044
Fergus Baker <fergusbkr@gmail.com>
2023-02-05 15:19:43 +0000

    Fix: allow stop eating (#65)

--- cart/world.go
+++ cart/world.go
@@ -63,6 +63,16 @@ func (world *World) DigTree(x, y int32, game *Game) {
 	}
 }
 
+func (world *World) Dig(x, y int32) {
+	tileIndex := world.GetMapTile(x, y)
+	switch {
+	case world.IsDirt(tileIndex):
+		world.DigTile(x, y)
+	case world.IsTree(tileIndex):
+		world.DigTree(x, y)
+	}
+}
+
 func (world *World) Draw() {
 	// The background is the entire right screenwidth of the map tiles
 	tic80.Map(WORLD_BACKGROUND_X, world.Y, 0, world.OffsetY)
